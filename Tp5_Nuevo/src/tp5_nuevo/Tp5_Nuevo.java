10905323a0cdc7ba5717e2fc17eca6421d56ce84
LucasOsella <lucasosella01@gmail.com>
2024-11-08 14:53:24 -0300

    Prueba de metodos

--- Tp5_Nuevo/src/tp5_nuevo/Tp5_Nuevo.java
+++ Tp5_Nuevo/src/tp5_nuevo/Tp5_Nuevo.java
@@ -16,6 +16,14 @@ public class Tp5_Nuevo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Conversor con = new Conversor(2000);
+        //System.out.println("Su sueldo convertido a pesos es igual a "+con.convertir(2000, 993.74));
+        //System.out.println("Cotizacion "+con.calcularCotizacion(1650, 1800000));
+          con.aumentarSaldo(500);
+//        System.out.println("Su saldo actual es de "+con.getSaldoUSD());
+          con.retirarFisico(100);
+          System.out.println("Su saldo despues de retirar es de "+con.getSaldoUSD());
+            
     }
     
 }
